bba7490657287601574c404c3be2b22a1de2dc3a
RicardooVidal <ricardoorv95@gmail.com>
2024-09-25 21:59:35 -0300

    add sanctum authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken($user->name, ['*'], now()->addSecond())->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        if ($user) {
+            return response()->json(['token' => $user->createToken($user->name)->plainTextToken]);
+        }
+
+        return response()->json(['error' => 'Error creating user'], 500);
+    }
+}
